30dba79d27904d0f3d340e53cc21f2245d5c4c34
Dylan Sullivan <dvs1@txstate.edu>
2017-04-23 13:14:40 -0500

    Update mysh.c
    I have most of the shell done it stills needs pipeing for the cat command and maybe redirection for stdin, stdout, and stder.

--- mysh.c
+++ mysh.c
@@ -2,55 +2,68 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 //This is not completed yet!
+// ERRORS
+//  ././ can execute the execution!!
 
-void sh_split(char *line)
+void sh_split(char *line, char **array)
 {
-
+    //char buf[] ="abc qwe ccd";
     int i = 0;
     char *p = strtok (line, " ");
-    char *array[10];
+    //char *array[10];
 
 
+	//message = strcat("TEXT " , var);
+	
     while (p != NULL)
     {
+		//had the argument had a dollar sign in front of it we would need to extract the argument out.
+		if(p[0] == '>') {
+			printf("%s\n", p); 
+
+			int fd=open("afile2.txt", O_WRONLY|O_CREAT|O_TRUNC, 0600);
+			
+			if (fd < 0){ exit(1);}
+			dup2(fd, 1);
+			close(fd);
+			
+		}
+		else {
         array[i++] = p;
+		}
         p = strtok (NULL, " ");
+		
     }
 	
-	//printf("%d\n", i);
-
-    for (int b = 0; b < 3; ++b) 
-        printf("%s\n", array[b]);
-
 	array[i] = NULL;
-	//i = i - 1;
 	
+}
+
+void sh_execute(char **array)
+{
 
 		pid_t pid=fork();
-    if (pid==0) { /* child process */
+    if (pid==0) { //child process
 		//char *cmd = "./";
 		char cmd[50];
 		strcpy(cmd, "./");
 		strcat(cmd, array[0]);
-		
-		//printf("%s\n",cmd);
-		execvp(cmd, array);
-    }
-    else { /* pid!=0; parent process */
-        waitpid(pid,0,0); /* wait for child to exit */
-    }
+				
 
-    //return 0;
-}
-
-void sh_execute()
-{
-	//not using this function yet
+		if(	execvp(cmd, array) == -1){
+			perror("error executing");
+		}
+	
+    }
+    else { // pid!=0; parent process *
+        waitpid(pid,0,0); // wait for child to exit 
+    }
 }
 
 char *loop(void)
@@ -61,28 +74,39 @@ char *loop(void)
     if (getline(&line, &size, stdin) == -1) {
         printf("No line\n");
     } else {
+	
 	return line;
         //printf("%s\n", line);
     }
 	
 }
 
-static char* skipwhite(char* b)
-{
-	//strchr(cmd, ' '); think you can use this to test
-	//while (test for spaces here) ++s;
-	return b;
-}
-
-int main(int argc, char **argv)
+int main()
 {
   	printf("$ ");
 	char *line;
+	char *array[10]; //prolly need to check for number of arguments later.
+	while(1) {
+	
  	// Run command loop.
   	line = loop();
 
-	sh_split(line);
-	//sh_execute();    
+	
+	sh_split(line, array);
+	
+	int ret = strcmp(array[0], "exit\n");
+
+
+	if(strcmp(array[0], "exit\n") == 0){ 
+		printf("this is exit\n");
+		exit(0);}
+	
+	sh_execute(array);
+
 	free(line);
+
+	}
+	//sh_execute();    
+	
   	return 0;
 }
