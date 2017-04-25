ffb7794f755d0aca8e1780261b9d6a0525c8babd
Dylan Sullivan <dvs1@txstate.edu>
2017-04-25 00:04:42 -0500

    Update mysh.c
    Most everything works like he said know. The only thing Im not sure is if you need to add stdin, stdout, and stderror.

--- mysh.c
+++ mysh.c
@@ -8,19 +8,16 @@
 #include <string.h>
 
 
-int saved_stdout;
+int saved_stdout;//For save the standard out
 int sh_stoutput(){
 	return saved_stdout;
 }
 
-
 void sh_split(char *line, char **array)
 {
-	
     int i = 0; //This is an index for the array
-	
     char *p = strtok (line, " ");
-
+	char *c;//just points to the same place as p
 	
     while (p != NULL)
     {
@@ -35,17 +32,18 @@ void sh_split(char *line, char **array)
 		}
 		else if(p[0] == '<') //This is for redirection
 		{	
-			//++p;
-			//array[i++] = p;
+			strcpy(c,c + 1);	
+			array[i++] = c;
 		}
 		else {
        	 	array[i++] = p;
 		}
       	 	p = strtok (NULL, " ");
+			c = p;
     }
 		
 	if( i == 3 ){
-		if(array[0] != NULL && array[1][0] == '|' && array[2] != NULL)
+		if(array[0] != NULL && array[1][0] == '|' && array[2] != NULL)//for piping
 		{
 		int pid1, pid2;
 		int pp[2];
@@ -55,15 +53,21 @@ void sh_split(char *line, char **array)
 		dup2(pp[1], 1);
 		close(pp[0]);
 		close(pp[1]);
-
-		char *cmd = "./myls";
+		//Check for which type of command to execute.
+		char cmd[50];
+		if(array[0][0] == 'm' && array[0][1] == 'y'){
+			strcpy(cmd, "./");
+			strcat(cmd, array[0]);	}
+		else {
+			strcpy(cmd, array[0]);	
+		}	
 		char *argv[2];
-		argv[0] = "ls";
+		argv[0] = array[0];
 		argv[1] = NULL;
 		execvp(cmd, argv);
 		}
 		pid2=fork();
-		if (pid2==0) { // execute foo15a
+		if (pid2==0) { // execute the pipeline
 		dup2(pp[0], 0);
 		close(pp[0]);
 		close(pp[1]);
@@ -76,32 +80,26 @@ void sh_split(char *line, char **array)
 		}
 		close(pp[0]);
 		close(pp[1]);
-
+		array[0] = "stop";
 		}
 	}
 
 	if(i == 0){
-
 		i++;
-		array[0] = "0";
+		array[0] = "0";//Set to 0 so it does not try to execute
 	}
-
-
 	array[i] = NULL;
-
-
-	
 	
 }
 
 void sh_execute(char **array)
 {
 
-			pid_t pid=fork();
-    if (pid==0 && array[0] != "exit" && array[0] != "0") { //child process
+	pid_t pid=fork();
+    if (pid==0 && array[0] != "stop" && array[0] != "0") { //child process
 		//char *cmd = "./";
 		char cmd[50];
-		//*****
+		//check for regular or my commands
 		if(array[0][0] == 'm' && array[0][1] == 'y'){
 			strcpy(cmd, "./");
 			strcat(cmd, array[0]);	}
@@ -127,13 +125,13 @@ void sh_execute(char **array)
 
 char *line2(void)
 {
-
+	
     char *line = NULL;
 	size_t size;
 	//this should get the line and the size of the address.
 	int p = getline(&line, &size, stdin);
     if (p == -1) {
-       printf("No line\n");
+       printf("No line\n"); exit(0);
     } else if (p == 1){
 		
 		return NULL;
@@ -142,7 +140,7 @@ char *line2(void)
 	strtok(line, "\n"); //remove a null character
 	return line;
     }
-	
+	return line;
 }
 
 
@@ -150,9 +148,8 @@ int main()
 {
 	saved_stdout = dup(1);
 	char *line;
-	char *array[10]; //prolly need to check for number of arguments later.
+	char *array[10]; //it only reads 10
 	while(1) {
-	
  	// Run command loop.
   	line = line2();
 
@@ -163,13 +160,9 @@ int main()
 		exit(0);
 		}
 	
-
-
 	sh_execute(array);
 
 	free(line);
 	}
-	 
-	
   	return 0;
 }
